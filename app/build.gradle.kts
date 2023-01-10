02af628aaba1c9ee625f119f6b2d2181656d7049
antoniolg <antonio@devexperto.com>
2023-01-10 11:35:25 +0100

    Unit tests for repositories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,11 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.github.bumptech.glide:glide:4.15.1")
     ksp("com.github.bumptech.glide:compiler:4.15.1")
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
