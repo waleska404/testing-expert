434cfdc6cfcdcd2da919ac169a871822ee69a1ce
antoniolg <antonio@devexperto.com>
2023-01-10 13:22:17 +0100

    Migrate to MockK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,7 @@ dependencies {
     ksp("com.github.bumptech.glide:compiler:4.15.1")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
-    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("io.mockk:mockk:1.13.3")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
