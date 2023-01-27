337a571c694d6de5fc67aac74b03518f9712ca2c
antoniolg <antonio@devexperto.com>
2023-01-27 09:58:15 +0100

    Compose Tests - Board

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,14 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/{AL2.0,LGPL2.1,LICENSE-notice.md,LICENSE.md}"
+        }
+    }
+    testOptions {
+        packaging {
+            jniLibs {
+                useLegacyPackaging = true
+            }
         }
     }
 }
@@ -105,6 +112,7 @@ dependencies {
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.46.1")
     androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
     androidTestImplementation("app.cash.turbine:turbine:1.0.0")
+    androidTestImplementation("io.mockk:mockk-android:1.13.3")
 
     debugImplementation("androidx.fragment:fragment-testing-manifest:1.6.0")
     androidTestImplementation("androidx.fragment:fragment-testing:1.6.0")
