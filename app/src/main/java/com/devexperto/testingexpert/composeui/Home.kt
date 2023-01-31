63e6c0308c956f9203ff634505b9680ffe284fca
antoniolg <antonio@devexperto.com>
2023-01-31 01:11:23 +0100

    Build Theme change using TDD

--- app/src/main/java/com/devexperto/testingexpert/composeui/Home.kt
+++ app/src/main/java/com/devexperto/testingexpert/composeui/Home.kt
@@ -4,42 +4,48 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.devexperto.testingexpert.R
 import com.devexperto.testingexpert.composeui.navigation.NavItem
 import com.devexperto.testingexpert.composeui.navigation.Navigation
+import com.devexperto.testingexpert.composeui.navigation.TopBar
+import com.devexperto.testingexpert.composeui.settings.SettingsViewModel
+import com.devexperto.testingexpert.composeui.settings.Theme
 
 @Composable
-fun Home() {
-
-    val navController = rememberNavController()
-    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route ?: ""
-
-    Scaffold(
-        topBar = { TopBar() },
-        bottomBar = {
-            BottomNavBar(
-                currentRoute = currentRoute,
-                onNavItemClick = navController::navigate
-            )
-        }
-    ) { padding ->
-        Box(modifier = Modifier.padding(padding)) {
-            Navigation(navController)
-        }
-    }
+fun Home(vm: SettingsViewModel = hiltViewModel()) {
+    val theme by vm.theme.collectAsState(initial = Theme.SAME_AS_SYSTEM)
+    Home(theme)
 }
 
 @Composable
-fun TopBar() {
-    TopAppBar(
-        title = { Text(text = stringResource(id = R.string.app_name)) }
-    )
+fun Home(theme: Theme) {
+    Screen(theme) {
+        val navController = rememberNavController()
+        val currentRoute =
+            navController.currentBackStackEntryAsState().value?.destination?.route ?: ""
+
+        Scaffold(
+            topBar = { TopBar() },
+            bottomBar = {
+                BottomNavBar(
+                    currentRoute = currentRoute,
+                    onNavItemClick = navController::navigate
+                )
+            }
+        ) { padding ->
+            Box(modifier = Modifier.padding(padding)) {
+                Navigation(navController)
+            }
+        }
+    }
 }
 
 @Composable
