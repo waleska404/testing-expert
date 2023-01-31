d59933c9e82739dbfd2bf6fd531650836ebb14da
antoniolg <antonio@devexperto.com>
2023-01-31 10:57:38 +0100

    Convert current tests to JUnit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,10 +68,19 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.0")
     testImplementation("app.cash.turbine:turbine:1.0.0")
 
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 kapt {
     correctErrorTypes = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
