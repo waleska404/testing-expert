8044b5f18a5c56242ebb3e10fc7185be4dd5ed71
antoniolg <antonio@devexperto.com>
2023-05-16 01:02:42 +0200

    Configure instrumented tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.0")
     testImplementation("app.cash.turbine:turbine:1.0.0")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
 
 kapt {
